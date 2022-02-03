cc78fec7942040b6fc5a55cc71daf9a1175695a7
Ausra Miksyte <ausra.miksyte@baringa.com>
2022-02-03 05:30:55 +0000

    README update, remove secret key from settings

--- backend/demoapp/settings.py
+++ backend/demoapp/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i3(ez_&pmk+mogn))*7j9cf^1i-e$qg5340##4^#d8aibdkbeq'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
