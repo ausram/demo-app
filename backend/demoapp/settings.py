34aa2dd3a99bbe814dfd837ee386d8f30f560d47
Ausra Miksyte <ausra.miksyte@baringa.com>
2022-02-03 15:16:50 +0000

    Add env variables package

--- backend/demoapp/settings.py
+++ backend/demoapp/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
